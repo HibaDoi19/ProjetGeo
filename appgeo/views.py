1daff3a59d748fe7396ad96e046d4e274e6676ae
Hibadoi19 <Inscriptionhiba2019@gmail.com>
2022-01-08 21:04:28 +0100

    correct version of direct & inverse

--- appgeo/views.py
+++ appgeo/views.py
@@ -1,3 +1,4 @@
+
 from typing import Text
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -14,64 +15,69 @@ import os
 
 
 def direct(latitude1, longitude1, alpha1, s, a, b):
-                if a<0 or b<0:
-                        texte="a et b doivent être positifs"
-                        return texte
-                else:
-                        f = (a-b)/a
-                        ep = sqrt((a**2-b**2)/b**2)
-                        #Calcul de la latitude réduite beta1
-                        TanBeta1 = (1-f) * tan(latitude1)
-                        Beta1 = atan( TanBeta1 )
-                        #Calcul de la latitude réduite beta0
-                        CosBeta0 = cos(Beta1)*sin(alpha1)
-                        Beta0 = acos(CosBeta0)
-                        #Calcul de la constante w2
-                        w2 = (sin(Beta0)*ep)*2
-                        #Calcul de la distance angulaire sigma1 sur la sphère auxiliaire
-                        sigma1 = atan2( TanBeta1, cos(alpha1) )
-                        #Calcul de l'azimut de la géodésique à l'équateur alphae
-                        Sinalphae = CosBeta0
-                        alphae = asin(Sinalphae)
-                        cosalphae_2= 1.0 - Sinalphae**2
-                        #Calcul des constantes de Vincenty A' et B'
-                        A = 1.0 + (w2 / 16384) * (4096 + w2 * (-768 + w2 * (320 - 175 * w2) ) )
-                        B = (w2 / 1024) * (256 + w2 * (-128 + w2 * (74 - 47 * w2) ) )
-                        #Calcul de la distance angulaire sigma sur le grand cercle de la sphère auxiliaire
-                        sigma0 = (s / (b * A))
+        if a<0 or b<0:
+                texte="a et b doivent être positifs"
+                return texte
+        else:
+                f = (a-b)/a
+                ep = sqrt((a**2-b**2)/b**2)
+                #Calcul de la latitude réduite beta1
+                TanBeta1 = (1-f) * tan(latitude1)
+                Beta1 = atan( TanBeta1 )
+                #Calcul de la latitude réduite beta0
+                CosBeta0 = cos(Beta1)*sin(alpha1)
+                Beta0 = acos(CosBeta0)
+                #Calcul de la constante w2
+                w2 = (sin(Beta0)*ep)**2
+                #Calcul de la distance angulaire sigma1 sur la sphère auxiliaire
+                sigma1 = atan2( TanBeta1, cos(alpha1) )
+                #Calcul de l'azimut de la géodésique à l'équateur alphae
+                Sinalphae = CosBeta0
+                alphae = asin(Sinalphae)
+                cosalphae_2= 1.0 - Sinalphae**2
+                #Calcul des constantes de Vincenty A' et B'
+                A = 1.0 + (w2 / 16384) * (4096 + w2 * (-768 + w2 * (320 - 175 * w2) ) )
+                B = (w2 / 1024) * (256 + w2 * (-128 + w2 * (74 - 47 * w2) ) )
+                #Calcul de la distance angulaire sigma sur le grand cercle de la sphère auxiliaire
+                sigma0 = (s / (b * A))
+                sigma_m2 = (2 * sigma1 + sigma0)
+                delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0)*(-1 + 2 * pow( cos(sigma_m2), 2 ) - (B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
+                sigma = sigma0  + delta_sigma
+                while ( abs(sigma-sigma0)>0.00001) :
+                        sigma0=sigma
                         sigma_m2 = (2 * sigma1 + sigma0)
-                        delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0)*(-1 + 2 * pow( cos(sigma_m2), 2 ) - (B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
-                        sigma = sigma0  + delta_sigma
-                        while ( abs(sigma-sigma0)>0.00001) :
-                                sigma0=sigma
-                                sigma_m2 = (2 * sigma1 + sigma0)
-                                delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0) *(-1 + 2 * pow( cos(sigma_m2), 2 ) -(B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
-                                sigma = (s / (b * A)) + delta_sigma
-                        #Calcul de la latitude 2
-                        TanBeta2=(sin(Beta1) * cos(sigma) + cos(Beta1) * sin(sigma) * cos(alpha1))/(sqrt( pow(Sinalphae, 2)+pow(sin(Beta1) * sin(sigma) - cos(Beta1) * cos(sigma) * cos(alpha1), 2)))
-                        latitude2 =atan(TanBeta2/(1-f))
-                        #Calcul de la différence de longitude sur la sphère auxiliaire deltau
-                        deltau=(sin(sigma)*sin(alpha1))/(cos(Beta1)*cos(sigma)-sin(Beta1)*sin(sigma)*cos(alpha1))
-                        #Calcul de la constante C de Vincenty
-                        C = (f/16) * cosalphae_2 * (4 + f * (4 - 3 * cosalphae_2 ))
-                        #Calcul de la différence de longitude dellambda
-                        deltalambda = deltau - (1-C) * f * Sinalphae *(sigma + C * sin(sigma) * (cos(sigma_m2) +C * cos(sigma) * (-1 + 2 * pow(cos(sigma_m2),2) )))
-                        longitude2 = longitude1 + deltalambda
-                        #Calcul de l'azimut alpha2 et de l'azimut inverse
-                        alpha2 = atan2 ( Sinalphae, (cos(Beta1) * cos(sigma) * cos(alpha1)-sin(Beta1) * sin(sigma)))
-                        if ( alpha2 < pi ) :
-                                alpha21 = alpha2 + pi
-                        if ( alpha2 > pi ) :
-                                alpha21 = alpha2 - pi
-                        #transformation des angles en degre
-                        latitude2= latitude2*180/pi
-                        longitude2= longitude2*180/pi
-                        alpha21 = alpha21*180/pi
-                        #if longitude2>180:
-                        #longitude2 = longitude2-360
-                        #if longitude2<0:
-                        # longitude2 = longitude2+360'''
-                        return round(longitude2), round(latitude2), round(alpha21)
+                        delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0) *(-1 + 2 * pow( cos(sigma_m2), 2 ) -(B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
+                        sigma = (s / (b * A)) + delta_sigma
+                #Calcul de la latitude 2
+                TanBeta2=(sin(Beta1) * cos(sigma) + cos(Beta1) * sin(sigma) * cos(alpha1))/(sqrt( pow(Sinalphae, 2)+pow(sin(Beta1) * sin(sigma) - cos(Beta1) * cos(sigma) * cos(alpha1), 2)))
+                latitude2 =atan(TanBeta2/(1-f))
+                #Calcul de la différence de longitude sur la sphère auxiliaire deltau
+                deltau=(sin(sigma)*sin(alpha1))/(cos(Beta1)*cos(sigma)-sin(Beta1)*sin(sigma)*cos(alpha1))
+                #Calcul de la constante C de Vincenty
+                C = (f/16) * cosalphae_2 * (4 + f * (4 - 3 * cosalphae_2 ))
+                #Calcul de la différence de longitude dellambda
+                deltalambda = deltau - (1-C) * f * Sinalphae *(sigma + C * sin(sigma) * (cos(sigma_m2) +C * cos(sigma) * (-1 + 2 * pow(cos(sigma_m2),2) )))
+                longitude2 = longitude1 + deltalambda
+
+                if longitude2<0 and latitude1>0: #condition ajouteé
+                        longitude2=longitude2+pi
+                elif longitude2<0 and latitude1<0:
+                        longitude2=longitude2
+                #Calcul de l'azimut alpha2 et de l'azimut inverse
+                alpha2 = atan2 ( Sinalphae, (cos(Beta1) * cos(sigma) * cos(alpha1)-sin(Beta1) * sin(sigma)))
+                if ( alpha2 < pi ) :
+                        alpha21 = alpha2 + pi
+                if ( alpha2 > pi ) :
+                        alpha21 = alpha2 - pi
+                #transformation des angles en degre
+                latitude2= latitude2*180/pi
+                longitude2= longitude2*180/pi
+                alpha21 = alpha21*180/pi
+                #if longitude2>180:
+                #longitude2 = longitude2-360
+                #if longitude2<0:
+                # longitude2 = longitude2+360
+                return round(longitude2), round(latitude2), round(alpha21)
 
 def say_hello(request):
         return render(request ,'direct.html')
@@ -108,72 +114,85 @@ def inverse(request):
                 if a<0 or b<0:
                         texte="a et b doivent être positifs"
                         return texte
-                        #hhhh
                 else:
-                        f = (a-b)/a		
-                        ep = sqrt((a*2-b2)/b*2)
-                        #Calcul de la latitude réduite beta1 
-                        TanBeta1 = (1-f) * tan(latitude1)
-                        Beta1 = atan( TanBeta1 )
-                        #Calcul de la latitude réduite beta0
-                        CosBeta0 = cos(Beta1)*sin(alpha1)
-                        Beta0 = acos(CosBeta0)
-                        #Calcul de la constante w2 
-                        w2 = (sin(Beta0)*ep)*2
-                        #Calcul de la distance angulaire sigma1 sur la sphère auxiliaire 
-                        sigma1 = atan2( TanBeta1, cos(alpha1) )
-                        #Calcul de l'azimut de la géodésique à l'équateur alphae
-                        Sinalphae = CosBeta0
-                        alphae = asin(Sinalphae)
-                        cosalphae_sq = 1.0 - Sinalphae**2
-                        #Calcul des constantes de Vincenty A' et B'
-                        A = 1.0 + (w2 / 16384) * (4096 + w2 * (-768 + w2 * (320 - 175 * w2) ) )
-                        B = (w2 / 1024) * (256 + w2 * (-128 + w2 * (74 - 47 * w2) ) )
-                        #Calcul de la distance angulaire sigma sur le grand cercle de la sphère auxiliaire 
-                        sigma = (s / (b * A))
-                        sigma_m2 = (2 * sigma1 + sigma)
-                        while ( abs(sigma-sigma0)>0.00001) :
-                                sigma_m2 = (2 * sigma1 + sigma)
-                                delta_sigma = B * sin(sigma) * ( cos(sigma_m2) 
-                                        + (B/4) * (cos(sigma) * 
-                                        (-1 + 2 * pow( cos(sigma_m2), 2 ) - 
-                                        (B/6) * cos(sigma_m2) * \
-                                        (-3 + 4 * pow(sin(sigma), 2 )) *  \
-                                        (-3 + 4 * pow( cos (sigma_m2), 2 ))))) 
-                                sigma = (s / (b * A)) + delta_sigma
-                        #Calcul de la latitude 2
-                        TanBeta2=(sin(Beta1) * cos(sigma) + cos(Beta1) * sin(sigma) * cos(alpha1)) \
-                        /(sqrt( pow(Sinalphae, 2)+pow(sin(Beta1) * sin(sigma) - cos(Beta1) * cos(sigma) * cos(alpha1), 2)))
-                        latitude2 =atan(TanBeta2/(1-f))
-                        #Calcul de la différence de longitude sur la sphère auxiliaire deltau
-                        deltau=(sin(sigma)*sin(alpha1))/(cos(Beta1)*cos(sigma)-sin(Beta1)*sin(sigma)*cos(alpha1))
-                        #Calcul de la constante C de Vincenty 
-                        C = (f/16) * cosalphae_sq * (4 + f * (4 - 3 * cosalphae_sq ))
-                        #Calcul de la différence de longitude dellambda
-                        deltalambda = deltau - (1-C) * f * Sinalphae *\
-                                (sigma + C * sin(sigma) * (cos(sigma_m) + \
-                                C * cos(sigma) * (-1 + 2 * pow(cos(sigma_m),2) )))
-                
-                        longitude2 = longitude1 + deltalambda
-                        #Calcul de l'azimut alpha2 et de l'azimut inverse 
-                        alpha2 = atan2 ( Sinalphae, (cos(Beta1) * cos(sigma) * cos(alpha1)\
-                                -sin(Beta1) * sin(sigma)))
-                
-                        alpha21 = alpha2 + 2*pi / 2.0
-                        if ( alpha21 < 0.0 ) :
-                                alpha21 = alpha21 + 2*pi
-                        if ( alpha21 > 2*pi ) :
-                                alpha21 = alpha21 - 2*pi
-                #transformation des angles en degre 
-                        latitude2= latitude2*180/pi
-                        longitude2= longitude2*180/pi
-                        alpha21 = alpha21*180/pi
-                        if longitude2>180:
-                                longitude2 = longitude2-360
-                        if longitude2<180:
-                                longitude2 = longitude2+360
-                        return round(longitude2), round(latitude2), round(alpha21)
+                        f = (a-b)/a
+                #condition sur phi1
+                if phi1 >(pi/2) or phi1<-(pi/2) :
+                        print('la valeur de la latitude doit être inférieur ou égal à 90°')
+                elif phi1==(pi/2) or phi1==-(pi/2):
+                        beta1=phi1
+                else:
+                        beta1=atan((1-f)*tan(phi1))
+                #condition sur phi2
+                if phi2 > (pi/2) or phi2 < -(pi/2):
+                        print('la valeur de la latitude doit être inférieur ou égal à 90°')
+                elif phi2 == (pi/2) or phi2 == -(pi/2):
+                        beta2=phi2
+                else:
+                        beta2=atan((1-f)*tan(phi2))
+                        #condition sur lam1
+                if lam1>pi or lam1<-pi :
+                        print('la valeur de la longétude doit être inférieur ou égal à 180°')
+                #condition sur lam2
+                if lam2>pi or lam2<-pi :
+                        print('la valeur de la longétude doit être inférieur ou égal à 180°')
+                #Calcul de la différence de longitude sur l ellipsoïde
+                delta_lam=lam2-lam1
+                delta_u0= delta_lam
+                #Calcul par itération
+                #calcul des grandeurs suivantes
+                sin2_sigma=pow(cos(beta2)*sin(delta_lam), 2) + pow((cos(beta1)*sin(beta2) - sin(beta1)*cos(beta2)*cos(delta_lam)),2)
+                Sin_sigma = sqrt(sin2_sigma )
+                Cos_sigma = sin(beta1) * sin(beta2) + cos(beta1)*cos(beta2)*cos(delta_lam)
+                sigma = atan2(Sin_sigma, Cos_sigma)
+                Sin_alphaE = cos(beta1)* cos(beta2)*sin(delta_lam)/ Sin_sigma
+                alphaE = asin(Sin_alphaE)
+                Cos2sigma_m = Cos_sigma - (2 * sin(beta1) * sin(beta2) / pow(cos(alphaE), 2) )
+                #C Constante de Vincenty
+                C = (f/16) * pow(cos(alphaE), 2) * (4 + f * (4 - 3 * pow(cos(alphaE), 2)))
+                delta_u = delta_lam + (1-C)*f* sin(alphaE)*(sigma + C * Sin_sigma *(Cos2sigma_m + C* Cos_sigma *(-1 + 2 * pow(Cos2sigma_m, 2))))
+                while abs(delta_u - delta_u0)>1.0e-5:
+                        # calcul des grandeurs suivantes
+                        sin2_sigma = pow(cos(beta2) * sin(delta_u), 2) + pow((cos(beta1) * sin(beta2) - sin(beta1) * cos(beta2) * cos(delta_u)), 2)
+                        Sin_sigma = sqrt(sin2_sigma)
+                        Cos_sigma = sin(beta1) * sin(beta2) + cos(beta1) * cos(beta2) * cos(delta_u)
+                        sigma = atan2(Sin_sigma, Cos_sigma)
+                        Sin_alphaE = cos(beta1) * cos(beta2) * sin(delta_u) / Sin_sigma
+                        alphaE = asin(Sin_alphaE)
+                        Cos2sigma_m = Cos_sigma - (2 * sin(beta1) * sin(beta2) / pow(cos(alphaE), 2))
+                        # C Constante de Vincenty
+                        C = (f / 16) * pow(cos(alphaE), 2) * (4 + f * (4 - 3 * pow(cos(alphaE), 2)))
+                        delta_u0 = delta_u
+                        delta_u= delta_lam + (1 - C) * f * sin(alphaE) * (sigma + C * Sin_sigma * (Cos2sigma_m + C * Cos_sigma * (-1 + 2 * pow(Cos2sigma_m, 2))))
+                #Calcul de la latitude réduite de vertex de la géodésie
+                beta0=acos(Sin_alphaE)
+                #Calcul de la constante w_carre
+                w_carre =((a*a-b*b) / (b*b))*pow(sin(beta0),2)
+                #Calcul des constantes de Vincenty A' et B'
+                A = 1 + (w_carre/16384) * (4096 + w_carre * (-768 + w_carre * (320 - 175 * w_carre)))
+                B = (w_carre/1024) * (256 + w_carre * (-128+ w_carre * (74 - 47 * w_carre)))
+                #Calcul de delta_sigma
+                delta_sigma = B * Sin_sigma * (Cos2sigma_m + (B/4)*(Cos_sigma * (-1 + 2 * pow(Cos2sigma_m, 2) )-(B/6) * Cos2sigma_m * (-3 + 4 *sin2_sigma)*(-3 + 4 * pow(Cos2sigma_m,2 ) )))
+                #Calcul de la distance géodésique s
+                s = b * A * (sigma - delta_sigma)
+                #Calcul de l'azimut directe alpha12
+                alpha12 = atan2( (cos(beta2) * sin(delta_lam)),(cos(beta1) * sin(beta2) - sin(beta1) * cos(beta2) * cos(delta_lam)))
+                #Calcul de l'azimut inverse alpha21
+                alpha21 = atan2( (cos(beta1) * sin(delta_lam)),(-sin(beta1) * cos(beta2) + cos(beta1) * sin(beta2) * cos(delta_lam)))
+                if ( alpha12 < 0.0 ) :
+                        alpha12 =  alpha12 + 2*pi
+                if ( alpha12 > 2*pi ) :
+                        alpha12 = alpha12 - 2*pi
+                alpha21 = alpha21 + 2*pi / 2.0
+                if ( alpha21 < 0.0 ) :
+                        alpha21 = alpha21 + 2*pi
+                if ( alpha21 > 2*pi ) :
+                        alpha21 = alpha21 - 2*pi
 
+                alpha12 = alpha12*180/pi
+                alpha21 = alpha21*180/pi
+                return alpha12, alpha21, s
+                #return round(alpha12), round(alpha21), round(s)
 
                 """f = (a-b)/a
                 if phi1==90 or phi1==-90:
@@ -380,18 +399,6 @@ def ellipsoide(longitude1, latitude1,alpha1,s,a,b):
         plot_div = plot(fig, output_type='div', include_plotlyjs=False)
         return plot_div
         
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
