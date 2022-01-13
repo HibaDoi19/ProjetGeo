563c4a8ffd6c683ac810116b02f0379c03eec8d9
Hibadoi19 <inscriptionhiba2019@gmail.com>
2022-01-13 15:09:02 +0100

    success visualisation

--- appgeo/views.py
+++ appgeo/views.py
@@ -1,17 +1,20 @@
 
+from re import template
 from typing import Text
+from _plotly_utils.utils import template_doc
 from django.shortcuts import render
 from django.http import HttpResponse
 from math import *
-from plotly.offline import iplot, init_notebook_mode, plot
+from plotly.offline import iplot, init_notebook_mode, plot 
 import plotly.graph_objects as go
-from numpy import sin, cos, pi
+from numpy import FLOATING_POINT_SUPPORT, sin, cos, pi
 import  numpy as np
 import plotly.express as px 
 import datetime
 import glob
 import logging
 import os
+from . import ploot
 
 def direct(latitude1, longitude1, alpha1, s, a, b):
 
@@ -108,112 +111,10 @@ def direct(latitude1, longitude1, alpha1, s, a, b):
         #if longitude2<0:
             # longitude2 = longitude2+360
         return round(longitude2), round(latitude2),round(alpha21)
-def visualisation(longitude1 , latitude1,alpha1,s,a,b):
-        #visualisation de l'ellipsoide
-        n=500
-        t=s/(n-1)
-        az_dir=[]
-        lat=[]
-        long=[]
-        az_dir.append(alpha1)
-        lat.append(latitude1)
-        long.append(longitude1) 
-        for i in range(1,n) :
-                aa,bb,cc=directe(latitude1,longitude1,alpha1,t,a,b)
-                lat.append(bb)
-                long.append(aa)
-                az_dir.append(cc)
-                alpha1=az_dir[i]
-                longitude1=long[i] 
-                latitude1=lat[i]
-        x=[]
-        y=[]
-        z=[]
-        for i in range(0,n): 
-                x.append((a*cos(long[i])*cos(lat[i]))/sqrt(1-(a**2-b**2)/(b**2)*(sin(lat[i]))**2))
-                y.append((a*sin(long[i])*cos(lat[i]))/sqrt(1-(a**2-b**2)/(b**2)*(sin(lat[i]))**2))
-                z.append((a*(1-(a**2-b**2)/(b**2))*sin(lat[i]))/sqrt(1-(a**2-b**2)/(b**2)*(sin(lat[i]))**2))
-        
-        return x,y,z 
-def directe(latitude1, longitude1, alpha1, s, a, b):
-    #VERIFICATION DES PARAMèTRE DE L'ELLIPSOIDE
-        if a<0 or b<0 or b>a:
-                texte="a et b doivent être positifs et b doit être inférieur à a"
-                return texte
-        #vérification de s
-        elif s<0 :
-                texte="erreur, distance doit être positive"
-                return texte
-        #vérification de l'azimut
-        elif alpha1<0 or alpha1>360 :
-                texte="Erreur : azimut doit être entre 0° et 360°"
-                return texte
-        elif abs(latitude1)>pi/2 :
-                texte="Erreur: valeur de la latitude hors rang"
-                return texte
-        elif abs(longitude1)>pi :
-                texte= "erreur : longitude hors rang ! "
-                return texte
-        else:
-                f = (a-b)/a
-                ep = sqrt((a**2-b**2)/b**2)
-                #Calcul de la latitude réduite beta1
-                if (abs(latitude1) != pi/2) :
-                        TanBeta1 = (1-f) * tan(latitude1)
-                        Beta1 = atan(TanBeta1 )
-                else:
-                        Beta1=latitude1
-                #Calcul de la latitude réduite beta0
-                CosBeta0 = cos(Beta1)*sin(alpha1)
-                Beta0 = acos(CosBeta0)
-                #Calcul de la constante w2
-                w2 = (sin(Beta0)*ep)**2
-                #Calcul de la distance angulaire sigma1 sur la sphère auxiliaire
-                if latitude1 == pi/2 or latitude1 == -pi/2 :
-                        sigma1=Beta1
-                elif alpha1==pi/2 or alpha1==3*pi/2 :
-                        if latitude1==0 :
-                                sigma1=0
-                        else :
-                                sigma1=pi/2
-                else:
-                        sigma1 = atan2( TanBeta1, cos(alpha1))
-                        #Calcul de l'azimut de la géodésique à l'équateur alphae
-                        Sinalphae = CosBeta0
-                        alphae = asin(Sinalphae)
-                        cosalphae_2= 1.0 - Sinalphae**2
-                        #Calcul des constantes de Vincenty A' et B'
-                        A = 1.0 + (w2 / 16384) * (4096 + w2 * (-768 + w2 * (320 - 175 * w2) ) )
-                        B = (w2 / 1024) * (256 + w2 * (-128 + w2 * (74 - 47 * w2) ) )
-                        #Calcul de la distance angulaire sigma sur le grand cercle de la sphère auxiliaire
-                        sigma0 = (s / (b * A))
-                        sigma_m2 = (2 * sigma1 + sigma0)
-                        delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0)*(-1 + 2 * pow( cos(sigma_m2), 2 ) - (B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
-                        sigma = sigma0  + delta_sigma
-                        while ( abs(sigma-sigma0)>0.00001) :
-                                sigma0=sigma
-                                sigma_m2 = (2 * sigma1 + sigma0)
-                                delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0) *(-1 + 2 * pow( cos(sigma_m2), 2 ) -(B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
-                                sigma = (s / (b * A)) + delta_sigma
-                        #Calcul de la latitude 2
-                        TanBeta2=(sin(Beta1) * cos(sigma) + cos(Beta1) * sin(sigma) * cos(alpha1))/(sqrt( pow(Sinalphae, 2)+pow(sin(Beta1) * sin(sigma) - cos(Beta1) * cos(sigma) * cos(alpha1), 2)))
-                        latitude2 =atan(TanBeta2/(1-f))
-                        #Calcul de la différence de longitude sur la sphère auxiliaire deltau
-                        deltau=atan((sin(sigma)*sin(alpha1))/(cos(Beta1)*cos(sigma)-sin(Beta1)*sin(sigma)*cos(alpha1)))
-                        #Calcul de la constante C de Vincenty
-                        C = (f/16) * cosalphae_2 * (4 + f * (4 - 3 * cosalphae_2 ))
-                        #Calcul de la différence de longitude dellambda
-                        deltalambda = deltau - (1-C) * f * Sinalphae *(sigma*pi/180 + C * sin(sigma) * (cos(sigma_m2) +C * cos(sigma) * (-1 + 2 * pow(cos(sigma_m2),2) )))
-                        longitude2 = (longitude1 + deltalambda)
-                        #Calcul de l'azimut alpha2 et de l'azimut inverse
-                        alpha2 = atan2 ( Sinalphae, (cos(Beta1) * cos(sigma) * cos(alpha1)-sin(Beta1) * sin(sigma)))
-                        alpha21=0
-                        if ( alpha2 < pi ) :
-                                alpha21 = alpha2 + pi
-                        if ( alpha2 > pi ) :
-                                alpha21 = alpha2 - pi
 
-                return longitude2, latitude2, alpha2
+        
+        
+
 
 def say_hello(request):
         return render(request ,'direct.html')
@@ -309,28 +210,9 @@ def add(request):
                 longitude1=longitude1*drc_long
                 #if request.methode =="post" and "Calculer" in request.POST :
                 res1,res2,res3=direct(latitude1, longitude1, alpha1, s, x, y)
-                        #res1=str(int(res1))+"°"
-                phi = np.linspace(0, 2*pi)
-                theta = np.linspace(-pi/2, pi/2)
-                phi, theta=np.meshgrid(phi, theta)
-
-                X = cos(theta) * sin(phi) * x
-                Y = cos(theta) * cos(phi) * x
-
-                Z = sin(theta)*y
-                layout = go.Layout(width = 700, height =700,
-                                        title_text='Géodesique')
-                fig = go.Figure(data=[go.Surface(x = X, y = Y, z=Z, colorscale = 'Blues')], layout=layout)
-
-                fig.update_traces(contours_z=dict(show=True, usecolormap=True,highlightcolor="limegreen", project_z=True))
-
-                x,y,z=visualisation(23*pi/180,33*pi/180,8*pi/180,2000000,6378137,6356752.314245)
-
-                fig.add_scatter3d(x=x,
-                                y=y,
-                                z=z,mode='lines',marker ={'color':'red'})
-                plot_div = plot(fig, output_type='div', include_plotlyjs=False)
-                return render( request   ,'result.html', {'result1': res1 ,'result2': res2 ,'result3': res3 } )
+                
+                
+                return render( request   ,'result.html', {'result1': res1 ,'result2': res2 ,'result3': res3 ,'plot':ploot.ellipsoide(longitude1,latitude1, alpha1, s, x, y)} )
                 
         
 
@@ -592,6 +474,8 @@ def directe(latitude1, longitude1, alpha1, s, a, b):
                         alpha21 = alpha2 - pi
 
                 return longitude2, latitude2, alpha2
+
+
 def visualisation(longitude1 , latitude1,alpha1,s,a,b):
         #visualisation de l'ellipsoide
         n=500
@@ -619,8 +503,48 @@ def visualisation(longitude1 , latitude1,alpha1,s,a,b):
                 z.append((a*(1-(a**2-b**2)/(b**2))*sin(lat[i]))/sqrt(1-(a**2-b**2)/(b**2)*(sin(lat[i]))**2))
         
         return x,y,z 
-#def ellipsoide(longitude1, latitude1,alpha1,s,a,b):
-        # some math: generate points on the surface of ellipsoid
+
+def ellitra(longitude1 , latitude1,alpha1,s,a,b):
+                phi = np.linspace(0, 2*pi)
+                theta = np.linspace(-pi/2, pi/2)
+                phi, theta=np.meshgrid(phi, theta)
+
+                X = cos(theta) * sin(phi) * a
+                Y = cos(theta) * cos(phi) * a
+
+                Z = sin(theta)*b
+                layout = go.Layout(width = 700, height =700,title_text='Géodesique')
+
+                fig = go.Figure(data=[go.Surface(x = X, y = Y, z=Z, colorscale = 'Blues')], layout=layout)
+
+                fig.update_traces(contours_z=dict(show=True, usecolormap=True,highlightcolor="limegreen", project_z=True))
+
+                x,y,z=visualisation(23*pi/180,33*pi/180,8*pi/180,2000000,6378137,6356752.314245)
+
+                return fig.add_scatter3d(x=x,y=y,z=z,mode='lines',marker ={'color':'red'})
+"""class Graph( template_doc):
+        template_name ='graph.html'
+        def get_context_data(self,**kwargs):
+                context = super(Graph , self ).get_context_data(**kwargs)
+                phi = np.linspace(0, 2*pi)
+                theta = np.linspace(-pi/2, pi/2)
+                phi, theta=np.meshgrid(phi, theta)
+
+                X = cos(theta) * sin(phi) * 6378137
+                Y = cos(theta) * cos(phi) * 6378137
+
+                Z = sin(theta)*6356752.314245
+                layout = go.Layout(width = 700, height =700,title_text='Géodesique')
+
+                fig = go.Figure(data=[go.Surface(x = X, y = Y, z=Z, colorscale = 'Blues')], layout=layout)
+
+                fig.update_traces(contours_z=dict(show=True, usecolormap=True,highlightcolor="limegreen", project_z=True))
+
+                x,y,z=visualisation(23*pi/180,33*pi/180,8*pi/180,2000000,6378137,6356752.314245)
+                fig.add_scatter3d(x=x,y=y,z=z,mode='lines',marker ={'color':'red'})
+                context['graph']= fig.to_html()
+                return context """
+                
 
 
         
