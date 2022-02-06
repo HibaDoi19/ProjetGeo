f25f8254eabc86d0d58e78d3cbd40f265bde7a2c
Hibadoi19 <inscriptionhiba2019@gmail.com>
2022-02-06 18:20:01 +0100

    err plot

--- appgeo/ploot.py
+++ appgeo/ploot.py
@@ -47,7 +47,7 @@ def visualisation(longitude1 , latitude1,alpha1,s,a,b,h):
         return x,y,z 
 
 def directe(latitude1, longitude1, alpha1, s, a, b):
-    #VERIFICATION DES PARAMèTRE DE L'ELLIPSOIDE
+        #VERIFICATION DES PARAMèTRE DE L'ELLIPSOIDE
 
                 f = (a-b)/a
                 ep = sqrt((a**2-b**2)/b**2)
@@ -72,53 +72,53 @@ def directe(latitude1, longitude1, alpha1, s, a, b):
                                 sigma1=pi/2
                 else:
                         sigma1 = atan2( TanBeta1, cos(alpha1))
-                        #Calcul de l'azimut de la géodésique à l'équateur alphae
-                        Sinalphae = CosBeta0
-                        alphae = asin(Sinalphae)
-                        cosalphae_2= 1.0 - Sinalphae**2
-                        #Calcul des constantes de Vincenty A' et B'
-                        A = 1.0 + (w2 / 16384) * (4096 + w2 * (-768 + w2 * (320 - 175 * w2) ) )
-                        B = (w2 / 1024) * (256 + w2 * (-128 + w2 * (74 - 47 * w2) ) )
-                        #Calcul de la distance angulaire sigma sur le grand cercle de la sphère auxiliaire
-                        sigma0 = (s / (b * A))
+                #Calcul de l'azimut de la géodésique à l'équateur alphae
+                Sinalphae = CosBeta0
+                alphae = asin(Sinalphae)
+                cosalphae_2= 1.0 - Sinalphae**2
+                #Calcul des constantes de Vincenty A' et B'
+                A = 1.0 + (w2 / 16384) * (4096 + w2 * (-768 + w2 * (320 - 175 * w2) ) )
+                B = (w2 / 1024) * (256 + w2 * (-128 + w2 * (74 - 47 * w2) ) )
+                #Calcul de la distance angulaire sigma sur le grand cercle de la sphère auxiliaire
+                sigma0 = (s / (b * A))
+                sigma_m2 = (2 * sigma1 + sigma0)
+                delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0)*(-1 + 2 * pow( cos(sigma_m2), 2 ) - (B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
+                sigma = sigma0  + delta_sigma
+                while ( abs(sigma-sigma0)>0.00001) :
+                        sigma0=sigma
                         sigma_m2 = (2 * sigma1 + sigma0)
-                        delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0)*(-1 + 2 * pow( cos(sigma_m2), 2 ) - (B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
-                        sigma = sigma0  + delta_sigma
-                        while ( abs(sigma-sigma0)>0.00001) :
-                                sigma0=sigma
-                                sigma_m2 = (2 * sigma1 + sigma0)
-                                delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0) *(-1 + 2 * pow( cos(sigma_m2), 2 ) -(B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
-                                sigma = (s / (b * A)) + delta_sigma
-                        #Calcul de la latitude 2
-                        TanBeta2=(sin(Beta1) * cos(sigma) + cos(Beta1) * sin(sigma) * cos(alpha1))/(sqrt( pow(Sinalphae, 2)+pow(sin(Beta1) * sin(sigma) - cos(Beta1) * cos(sigma) * cos(alpha1), 2)))
-                        latitude2 =atan(TanBeta2/(1-f))
-                        #Calcul de la différence de longitude sur la sphère auxiliaire deltau
-                        deltau=atan((sin(sigma)*sin(alpha1))/(cos(Beta1)*cos(sigma)-sin(Beta1)*sin(sigma)*cos(alpha1)))
-                        #Calcul de la constante C de Vincenty
-                        C = (f/16) * cosalphae_2 * (4 + f * (4 - 3 * cosalphae_2 ))
-                        #Calcul de la différence de longitude dellambda
-                        deltalambda = deltau - (1-C) * f * Sinalphae *(sigma*pi/180 + C * sin(sigma) * (cos(sigma_m2) +C * cos(sigma) * (-1 + 2 * pow(cos(sigma_m2),2) )))
-                        longitude2 = (longitude1 + deltalambda)
-                        #Calcul de l'azimut alpha2 et de l'azimut inverse
-                        alpha2 = atan2 ( Sinalphae, (cos(Beta1) * cos(sigma) * cos(alpha1)-sin(Beta1) * sin(sigma)))
-                        alpha21=0
-                        if ( alpha2 < pi ) :
-                                alpha21 = alpha2 + pi
-                        if ( alpha2 > pi ) :
-                                alpha21 = alpha2 - pi
-                        if alpha1<pi and longitude2<(longitude1*180/pi) and longitude1<0:
-                                longitude2 = longitude2+180
-                        if alpha1<pi and longitude1>0 and longitude2<(longitude1*180/pi) : 
-                                longitude2=longitude2-180  
-                        if alpha1>pi and longitude2>(longitude1*180/pi) and longitude1<0: 
-                                longitude2 = longitude2+180
-                        if alpha1>pi and longitude1>0 and longitude2>(longitude1*180/pi): 
-                                longitude2=longitude2+180
-                        if longitude2>180:
-                                longitude2=longitude2-360
-                        if longitude2<-180:
-                                longitude2=longitude2+360
-                                                
+                        delta_sigma = B * sin(sigma0) * ( cos(sigma_m2)+ (B/4) * (cos(sigma0) *(-1 + 2 * pow( cos(sigma_m2), 2 ) -(B/6) * cos(sigma_m2) *(-3 + 4 * pow(sin(sigma0), 2 )) *(-3 + 4 * pow( cos (sigma_m2), 2 )))))
+                        sigma = (s / (b * A)) + delta_sigma
+                #Calcul de la latitude 2
+                TanBeta2=(sin(Beta1) * cos(sigma) + cos(Beta1) * sin(sigma) * cos(alpha1))/(sqrt( pow(Sinalphae, 2)+pow(sin(Beta1) * sin(sigma) - cos(Beta1) * cos(sigma) * cos(alpha1), 2)))
+                latitude2 =atan(TanBeta2/(1-f))
+                #Calcul de la différence de longitude sur la sphère auxiliaire deltau
+                deltau=atan((sin(sigma)*sin(alpha1))/(cos(Beta1)*cos(sigma)-sin(Beta1)*sin(sigma)*cos(alpha1)))
+                #Calcul de la constante C de Vincenty
+                C = (f/16) * cosalphae_2 * (4 + f * (4 - 3 * cosalphae_2 ))
+                #Calcul de la différence de longitude dellambda
+                deltalambda = deltau - (1-C) * f * Sinalphae *(sigma*pi/180 + C * sin(sigma) * (cos(sigma_m2) +C * cos(sigma) * (-1 + 2 * pow(cos(sigma_m2),2) )))
+                longitude2 = (longitude1 + deltalambda)
+                #Calcul de l'azimut alpha2 et de l'azimut inverse
+                alpha2 = atan2 ( Sinalphae, (cos(Beta1) * cos(sigma) * cos(alpha1)-sin(Beta1) * sin(sigma)))
+                alpha21=0
+                if ( alpha2 < pi ) :
+                        alpha21 = alpha2 + pi
+                if ( alpha2 > pi ) :
+                        alpha21 = alpha2 - pi
+                if alpha1<pi and longitude2<(longitude1*180/pi) and longitude1<0:
+                        longitude2 = longitude2+180
+                if alpha1<pi and longitude1>0 and longitude2<(longitude1*180/pi) : 
+                        longitude2=longitude2-180  
+                if alpha1>pi and longitude2>(longitude1*180/pi) and longitude1<0: 
+                        longitude2 = longitude2+180
+                if alpha1>pi and longitude1>0 and longitude2>(longitude1*180/pi): 
+                        longitude2=longitude2+180
+                if longitude2>180:
+                        longitude2=longitude2-360
+                if longitude2<-180:
+                        longitude2=longitude2+360
+                                        
 
                 return longitude2,latitude2,alpha2
 
@@ -159,18 +159,30 @@ def ellipsoide(longitude1 , latitude1,alpha1,s,a,b,h):
                         
                         
     
-                        for i in np.linspace(-170,170,35):
-                                phi = np.linspace(i, i,100)
-                                theta = np.linspace(-180, 180,100)
-                                
-                                phi = phi*pi/180
-                                theta = theta*pi/180
-                                #phi, theta=np.meshgrid(phi, theta)
-                                x = cos(theta) * sin(phi) * a
-                                y = cos(theta) * cos(phi) * a
-                                z = sin(theta)*b
-                                t="longitude"+str(i)
-                                fig.add_scatter3d(x =x, y = y, z=z,mode='lines', marker={'color':'red'},text=t)
+                for i in [-170,-160,-150,-140,-130,-120,-110,-100,-90,-80,-70,-60,-50,-40,-30,-20,-10,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170]:
+                        phi = np.linspace(i, i,100)
+                        theta = np.linspace(-90,90,100)
+                        
+                        phi = phi*pi/180
+                        theta = theta*pi/180
+                        #phi, theta=np.meshgrid(phi, theta)
+                        x = cos(theta) * sin(phi) * a
+                        y = cos(theta) * cos(phi) * a
+                        z = sin(theta)*b
+                        t="longitude"+str(i)
+                        fig.add_scatter3d(x =x, y = y, z=z,mode='lines', marker={'color':'red'},text=t)
+                phi = np.linspace(0, 0,100)
+                theta = np.linspace(-90,90,100)
+                
+                phi = phi*pi/180
+                theta = theta*pi/180
+                #phi, theta=np.meshgrid(phi, theta)
+                x = cos(theta) * sin(phi) * a
+                y = cos(theta) * cos(phi) * a
+                z = sin(theta)*b
+                t="longitude"+str(i)
+                fig.add_scatter3d(x =x, y = y, z=z,mode='lines', marker={'color':'purple'},text=t)
+                        
                 ###########################################
 
                 plot_div = fig.to_html(full_html=False)
